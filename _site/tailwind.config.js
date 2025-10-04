f9bce2faffea874bb42605695e60c6800033337a
Dat Hoang <dathoangnd@gmail.com>
2025-10-04 18:14:33 +0700

    Upload built site.

---
+++ _site/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_bible/**/*.md",
+    "./*.html",
+    "./*.md"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
