38d2309154894cbadd59c8b574e96dd030fd6a2e
Dat Hoang <dathoangnd@gmail.com>
2025-10-04 18:28:13 +0700

    Remove _site.

--- _site/postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-    ...(env === 'production' ? { cssnano: { preset: 'default' } } : {})
-  }
-});
